e8beb6b2c74ee60c0c56fff59823ddbeddf1a06e
Phil <happy.melodist@gmail.com>
2021-11-10 17:35:20 -0600

    complete JS portion of the modal project

--- app.js
+++ app.js
@@ -9,3 +9,15 @@
 // closeBtn.addEventListener("click", function () {
 //   modalContainer.classList.remove("show");
 // });
+
+const openBtn = document.querySelector(".open");
+const modalContainer = document.querySelector(".modal-container");
+const closeBtn = document.querySelector(".modal-btn");
+
+openBtn.addEventListener("click", function () {
+  modalContainer.classList.add("show");
+});
+
+closeBtn.addEventListener("click", function () {
+  modalContainer.classList.remove("show");
+});
