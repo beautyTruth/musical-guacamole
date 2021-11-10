7871cb626f9b9a1ca103a306112af9a0f6b31003
Phil <happy.melodist@gmail.com>
2021-11-10 17:04:57 -0600

    complete overview of modal markup and css

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const openBtn = document.querySelector(".open");
-const modalContainer = document.querySelector(".modal-container");
-const closeBtn = document.querySelector(".modal-btn");
+// const openBtn = document.querySelector(".open");
+// const modalContainer = document.querySelector(".modal-container");
+// const closeBtn = document.querySelector(".modal-btn");
 
-openBtn.addEventListener("click", function () {
-  modalContainer.classList.add("show");
-});
+// openBtn.addEventListener("click", function () {
+//   modalContainer.classList.add("show");
+// });
 
-closeBtn.addEventListener("click", function () {
-  modalContainer.classList.remove("show");
-});
+// closeBtn.addEventListener("click", function () {
+//   modalContainer.classList.remove("show");
+// });
